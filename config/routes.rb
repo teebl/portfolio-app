166f26e1d3b394ff49b1914a2cf446c00217e1f4
Trevor Seibel <trevor.seibel@gmail.com>
2025-03-28 11:09:15 -0700

    projects setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources "blog", only: [ "index", "show" ]
+  resources "projects", only: [ "index", "show" ]
 
   get "about", to: "about#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
