96b2c8fa0dd167675fece9790076f2c4cd040a9a
Trevor Seibel <trevor.seibel@gmail.com>
2025-04-02 10:57:15 -0400

    featured project quick link

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,6 @@ class DashboardController < ApplicationController
     # This parses all the articles in order to get the most recent
     # Should performance become a problem, consider having the date in the filename, for faster sorting
     @latest_article =  parse_markdown_files("posts")[0]
+    @featured_project = parse_markdown_file("projects/project")
   end
 end
