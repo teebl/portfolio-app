bfb616034579180b1a291f80709c1b6276f481d1
Trevor Seibel <trevor.seibel@gmail.com>
2025-05-08 10:59:02 -0700

    change featured project

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,6 @@ class DashboardController < ApplicationController
     # This parses all the articles in order to get the most recent
     # Should performance become a problem, consider having the date in the filename, for faster sorting
     @latest_article =  parse_markdown_files("posts")[0]
-    @featured_project = parse_markdown_file("projects/old_portfolio_project")
+    @featured_project = parse_markdown_file("projects/view_attribute_merge")
   end
 end
